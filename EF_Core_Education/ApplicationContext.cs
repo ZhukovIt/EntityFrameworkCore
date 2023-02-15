a35ea12698ac69ee537b7eaf5640ec4967ab4e59
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-15 12:57:19 +0300

    Добавьте файлы проекта.

---
+++ EF_Core_Education/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core_Education
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ EF_Core_Education/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace EF_Core_Education
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User tom = new User { Name = "Tom", Age = 33 };
+                User alice = new User { Name = "Alice", Age = 26 };
+
+                db.Add(tom);
+                db.Add(alice);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены!");
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User user in users)
+                {
+                    Console.WriteLine($"{user.Id}. {user.Name} - {user.Age}");
+                }
+            }
+        }
+    }
+}
