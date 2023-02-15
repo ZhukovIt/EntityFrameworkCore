542aa8026993c0eaf4ff0e1c18d773b082e3bb2c
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-15 12:58:18 +0300

    Добавили новый проект для подключения к БД

---
+++ DbFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
